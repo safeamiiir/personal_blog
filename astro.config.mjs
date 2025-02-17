bcb3225d69fbfb58e4bcadf0e1d7ed53fdc7967a
safeamiiir <safehiana@gmail.com>
2025-02-17 00:22:16 +0000

    fix(stats): fix adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 /* 
   We are doing some URL mumbo jumbo here to tell Astro what the URL of your website will be.
@@ -36,4 +37,9 @@ export default defineConfig({
       config: { applyBaseStyles: false },
     }),
   ],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
