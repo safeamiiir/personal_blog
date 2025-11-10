31a4add5cef05fb78781f853f2d34a465dc465b4
safeamiiir <safehiana@gmail.com>
2025-11-10 15:24:38 +0000

    fix(favicon): make it done

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Not just another developer!";
+export const SITE_TITLE = "Not just another engineer!";
 export const SITE_DESCRIPTION =
   "Amirreza Safehian's Blog";
 export const TWITTER_HANDLE = "@amiiirsafe";
